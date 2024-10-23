2ab73cf9b064c98a3d0419a826676b7505b8cc00
JustAGameDev69 <134581425+JustAGameDev69@users.noreply.github.com>
2024-10-23 10:15:57 +0700

    Set up React Query

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -7,12 +7,22 @@ import Account from "./pages/Account";
 import Login from "./pages/Login";
 import PagenNotFound from "./pages/PageNotFound";
 import GlobalStyle from "./styles/GlobalStyle";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import AppLayout from "./ui/AppLayout";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    },
+  },
+});
 
 export default function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <GlobalStyle />
       <BrowserRouter>
         <Routes>
@@ -29,6 +39,7 @@ export default function App() {
           </Route>
         </Routes>
       </BrowserRouter>
-    </>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
--- the-wild-oasis/src/pages/Cabins.jsx
+++ the-wild-oasis/src/pages/Cabins.jsx
@@ -1,12 +1,19 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import CabinTable from "../features/cabins/CabinTable";
 
 function Cabins() {
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-    </Row>
+    <>
+      <Row type='horizontal'>
+        <Heading as='h1'>All cabins</Heading>
+        <p>Sorting</p>
+      </Row>
+
+      <Row type='vertical'>
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
