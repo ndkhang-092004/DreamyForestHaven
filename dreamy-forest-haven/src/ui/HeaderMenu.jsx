faa173ecfe082fba74a8ce18424a1b49052f25c3
JustAGameDev69 <vngkhang.icc@gmail.com>
2024-11-11 10:42:58 +0700

    Add Tooltip

--- dreamy-forest-haven/src/ui/HeaderMenu.jsx
+++ dreamy-forest-haven/src/ui/HeaderMenu.jsx
@@ -1,9 +1,11 @@
 import styled from "styled-components";
 import ButtonIcon from "./ButtonIcon";
 import Logout from "../features/authentication/Logout";
+import DarkModeToggle from "./DarkModeToggle";
 import { HiOutlineUser } from "react-icons/hi";
 import { useNavigate } from "react-router-dom";
-import DarkModeToggle from "./DarkModeToggle";
+import { Tooltip } from "react-tooltip";
+import { useDarkMode } from "../context/DarkModeContext";
 
 const StyledHeaderMenu = styled.ul`
   display: flex;
@@ -12,20 +14,43 @@ const StyledHeaderMenu = styled.ul`
 
 export default function HeaderMenu() {
   const navigate = useNavigate();
+  const { isDarkMode } = useDarkMode();
 
   return (
-    <StyledHeaderMenu>
-      <li>
-        <DarkModeToggle />
-      </li>
-      <li>
-        <ButtonIcon onClick={() => navigate("/account")}>
-          <HiOutlineUser />
-        </ButtonIcon>
-      </li>
-      <li>
-        <Logout />
-      </li>
-    </StyledHeaderMenu>
+    <>
+      <StyledHeaderMenu>
+        <li
+          data-tooltip-id='appearance-modes'
+          data-tooltip-content={isDarkMode ? "Light mode" : "Dark Mode"}
+          data-tooltip-place='bottom'
+          data-tooltip-variant={isDarkMode ? "dark" : "light"}
+        >
+          <DarkModeToggle />
+        </li>
+        <li
+          data-tooltip-id='account'
+          data-tooltip-content='Your Account'
+          data-tooltip-place='bottom'
+          data-tooltip-variant={isDarkMode ? "dark" : "light"}
+        >
+          <ButtonIcon onClick={() => navigate("/account")}>
+            <HiOutlineUser />
+          </ButtonIcon>
+        </li>
+        <li
+          data-tooltip-id='logout'
+          data-tooltip-content='Log Out'
+          data-tooltip-place='bottom'
+        >
+          <Logout />
+        </li>
+      </StyledHeaderMenu>
+      <Tooltip id='appearance-modes' border='2px solid rgb(222, 222, 222)' />
+      <Tooltip id='account' border='2px solid rgb(222, 222, 222)' />
+      <Tooltip
+        id='logout'
+        style={{ backgroundColor: "rgb(250, 23, 23)", color: "#ffffff" }}
+      />
+    </>
   );
 }
